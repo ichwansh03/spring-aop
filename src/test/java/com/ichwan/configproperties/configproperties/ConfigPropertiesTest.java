bf847118d1d289f800dee135a60164c66a8542bd
Ichwan <ichwansholihin03@gmail.com>
2023-11-06 17:14:20 +0700

    chore: add binding configuration properties

---
+++ src/test/java/com/ichwan/configproperties/configproperties/ConfigPropertiesTest.java
@@ -0,0 +1,28 @@
+package com.ichwan.configproperties.configproperties;
+
+import com.ichwan.configproperties.properties.ApplicationProperties;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ConfigPropertiesTest.TestApplication.class)
+public class ConfigPropertiesTest {
+
+    @Autowired
+    private ApplicationProperties properties;
+
+    @Test
+    void testConfigProperties() {
+        Assertions.assertEquals("Spring Boot Project", properties.getName());
+        Assertions.assertEquals(1, properties.getVersion());
+    }
+
+    @SpringBootApplication
+    @EnableConfigurationProperties({ApplicationProperties.class})
+    public static class TestApplication {
+
+    }
+}
