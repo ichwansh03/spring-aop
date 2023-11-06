211cdd5cc1dff509c73d3cbbcb4efd44d2fedf54
Ichwan <ichwansholihin03@gmail.com>
2023-11-06 09:31:52 +0700

    setup project

---
+++ src/main/java/com/ichwan/configproperties/SpringConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.ichwan.configproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigPropertiesApplication.class, args);
+	}
+
+}
